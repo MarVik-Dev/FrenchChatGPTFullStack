ade3d8a23559d4176045f48865b3a128ba1017ac
MarVic <55230624+MarVicFr@users.noreply.github.com>
2023-02-08 13:46:36 +0100

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 export const fetchResponse =  async(chat) => {
   try {
-      // after deployment NEED change the fetch URL below
-      const response = await fetch('http://localhost:3080', { 
+      // BEFORE deployment 'http://localhost:3080'
+      // after deployment 'https://french-chat-gpt-full-stack.vercel.app/'
+      const response = await fetch('https://french-chat-gpt-full-stack.vercel.app/', { 
           method: 'POST',
           headers: {
               "Content-Type": "application/json"
